0f3df3f9dc4b3a5568ac615b4c80e08c6519071c
Adrian Brügger <adrian.bruegger@imu.unibe.ch>
2022-08-12 16:55:59 +0200

    import data updated

--- code/01_import_clean_data.R
+++ code/01_import_clean_data.R
@@ -13,10 +13,16 @@ df %>% select(discuss_GW) %>% questionr::freq()
 
 
 # select variables
-
+df <- df %>% select(
+  discuss_GW, wave,happening,cause_original,sci_consensus,worry,
+  harm_personally,harm_US,harm_dev_countries,harm_future_gen,harm_plants_animals,when_harm_US,
+  gender, age, generation, educ_category, income_category, race, ideology, party,  region9,
+  service_attendance,marit_status,employment,house_size,house_type,house_own)
 
 # clean data, remove unnecessary levels
+df %>% psych::describe()
 
-
-# 
-
+# remove "-1" (refused) and "0" (don't know) level in all 
+df[df == -1] <- NA
+df[df == 0] <- NA
+df %>% psych::describe()
