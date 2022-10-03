38d35c93019f7cb2ef0ea6e70c2a56370c742271
Michael Schulte <michael@MacBook-Disaster-2.fritz.box>
2022-10-03 17:46:16 +0200

    sum across

--- code/01_import_clean_data.R
+++ code/01_import_clean_data.R
@@ -13,7 +13,7 @@ df %>% psych::describe()
 # remove "-1" (refused) and "0" (don't know) level in all 
 df[df == -1] <- NA
 df[df == 0] <- NA
-df %>% psych::describe()
+#df %>% psych::describe()
 
 df %>% select(discuss_GW) %>% questionr::freq()
 
@@ -37,6 +37,16 @@ df <- df %>% select(
   harm_personally,harm_US,harm_dev_countries,harm_future_gen,harm_plants_animals,when_harm_US,
   gender, age, generation, educ_category, income_category, race, ideology, party,  region9,
   service_attendance,marit_status,employment,house_size,house_type,house_own)
+
+df <- 
+df %>%
+  filter(!is.na(discuss_GW))
+
+df %>%
+  select(starts_with('harm')) %>%
+  is.na() %>%
+  tibble() %>% 
+  mutate(sum = rowSums(across(everything())))
 
 # resampling
 df_split <- rsample::initial_split(df, prop = 0.75, strata = discuss_GW)
--- code/02_feature_engineering.R
+++ code/02_feature_engineering.R
@@ -16,11 +16,17 @@ lm_fit <- logistic_model %>%
   fit(discuss_GW ~ ., data = df_training)
 
 # get parameters
-tidy(lm_fit) %>% print (n=30)
+tidy(lm_fit) %>% print(n=30)
 
-# make predictions
-df_predictions <- lm_fit %>%
-  predict(new_data = df_testing)
+# make classification predictions
+df_pred_class <- lm_fit %>%
+  predict(new_data = df_testing,
+          type = 'class')
+
+# make probability predictions
+df_pred_prob <- lm_fit %>%
+  predict(new_data = df_testing,
+          type = 'prob')
 
 # many NAs, probably because model returns NA when predictors are NA
 questionr::freq(df_training$discuss_GW)
@@ -28,9 +34,10 @@ questionr::freq(df_predictions)
 
 # add predicted values to dataset 
 df_test_results <- df_testing %>%
-  select(everything()) %>%
-  bind_cols(df_predictions)
+  select(discuss_GW) %>%
+  bind_cols(df_pred_class, df_pred_prob)
 
 # evaluate model performance
+sum(is.na(df_test_results$.pred_class))
 
 
